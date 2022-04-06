96f7c4e033c73c0543f4db5cadf79f5fc689ad08
waldronmatt <mhuntwaldron@gmail.com>
2022-04-05 22:08:08 -0400

    style(webpack.prod.js): remove comments

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -15,8 +15,6 @@ const productionConfig = {
           [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
         },
       },
-      // threshold: 8192, // only assets bigger than this size are processed
-      // minRatio: 0.8, // process if Compressed Size / Original Size < minRatio
     }),
     new ImageMinimizerPlugin({
       test: /\.(apng|avif|gif|jpe?g|png|svg|webp)$/i,
@@ -28,7 +26,6 @@ const productionConfig = {
       },
       generator: [
         {
-          // You can apply generator using `?as=webp`
           preset: 'webp',
           implementation: ImageMinimizerPlugin.imageminGenerate,
           options: {
@@ -40,32 +37,19 @@ const productionConfig = {
   ],
   optimization: {
     splitChunks: {
-      // tells SplitChunksPlugin to create chunks based on some conditions
       cacheGroups: {
         vendor: {
           name: 'vendors',
           chunks: 'all',
-          // import file path containing node_modules
           test: /node_modules/,
-          // Prioritize vendor chuncks over commons
           priority: 20,
         },
         common: {
           name: 'commons',
-          // minimum number of chunks that must share a module before splitting
           minChunks: 2,
           chunks: 'all',
-          // lower priority than vendors
           priority: 10,
-          /*
-            If the current chunk contains modules already split out from the
-            main bundle, it will be reused instead of a new one being generated.
-          */
           reuseExistingChunk: true,
-          /*
-            Enforce value is set to true to force SplitChunksPlugin to
-            form this chunk irrespective of the size of the chunk
-          */
           enforce: true,
         },
       },
