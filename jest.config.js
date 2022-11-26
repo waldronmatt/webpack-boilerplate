05a1449d7a6241a3fdedf8b92c66d2557c92e70c
waldronmatt <mhuntwaldron@gmail.com>
2022-11-26 16:39:40 -0500

    fix(global): upgrade jest, lint-staged, tsconfig configs

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,25 @@
 module.exports = {
   preset: '@waldronmatt/jest-config',
   testMatch: [
+    // other common folder naming conventions
+    // __mocks__ , __fixtures__ , __snapshots__
     '<rootDir>/__tests__/**/**.+(ts|tsx|js|jsx)',
     '<rootDir>/src/**/?(*.)+(spec|test).+(ts|tsx|js|jsx)',
   ],
+  moduleNameMapper: {
+    /*
+      method for stubbing Webpack resource queries
+      https://github.com/visualfanatic/vue-svg-loader/issues/73#issuecomment-478295746
+
+      should match any file extension containing any '?' resource
+      queries with '@' webpack alias
+    */
+    '^@/(.*.)(\\?(.*))$': '<rootDir>/src/$1',
+    // resolve '@' Webpack alias
+    // https://stackoverflow.com/a/65578141
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  // ignore transformations and coverages against node_modules
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  coveragePathIgnorePatterns: ['<rootDir>/node_modules/'],
 };
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,13 +1,23 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 const config = require('@waldronmatt/lint-staged-config');
-const ymlConfig = require('@waldronmatt/lint-staged-config');
+const ymlConfig = require('@waldronmatt/lint-staged-config/yml');
 
 module.exports = {
+  // check for credentials
   '*': ['secretlint'],
+  // ignore prettier on unknown extensions
   '!(*.{md,js,jsx,ts,tsx,json,css,scss,yml,yaml})': [
     'prettier --cache --write --ignore-unknown',
   ],
   ...ymlConfig,
   ...config,
+  // lint and fix changed markdown files
+  '*.md': ['prettier --cache --write', 'markdownlint'],
+  // lint and fix changed json files
+  '*.json': ['prettier --cache --write'],
+  // lint and fix changed css and scss files
+  '*.{css,scss}': ['prettier --cache --write', 'stylelint --cache --fix'],
+  // execute tests related to files changed in the current commit only
   '*.{js,jsx,ts,tsx}': [
     'yarn test --bail --passWithNoTests --findRelatedTests --coverage',
   ],
