c4d870654672f35a6a7e17777fac9a0e65e1fb82
waldronmatt <mhuntwaldron@gmail.com>
2023-03-25 23:29:36 -0400

    fix(webpack.prod.js): preload fonts and css

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -4,6 +4,8 @@ const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const zlib = require('node:zlib');
 const CopyPlugin = require('copy-webpack-plugin');
+// eslint-disable-next-line max-len
+const HtmlWebpackInjectPreload = require('@principalstudio/html-webpack-inject-preload');
 const { extendWebpackBaseConfig } = require('@waldronmatt/webpack-config');
 const commonConfig = require('./webpack.common');
 const paths = require('./paths');
@@ -13,6 +15,18 @@ const productionConfig = {
     new ESBuildMinifyPlugin({
       target: 'es2015',
       css: true,
+    }),
+    new HtmlWebpackInjectPreload({
+      files: [
+        {
+          match: /.*\.woff2?$/,
+          attributes: { as: 'font', type: 'font/woff2', crossorigin: true },
+        },
+        {
+          match: /.[\da-z-]*.css$/,
+          attributes: { as: 'style' },
+        },
+      ],
     }),
     new ImageMinimizerPlugin({
       test: /\.(apng|avif|gif|jpe?g|png|svg|webp)$/i,
